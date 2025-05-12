041275eaf03de8b9a637f67338d40072ac4108ee
burgosfacundo <facundoandresbur@hotmail.com>
2025-05-12 13:46:26 -0300

    init project

---
+++ mordisco-api/src/test/java/utn/back/mordiscoapi/MordiscoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.back.mordiscoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MordiscoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
