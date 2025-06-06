5d5bdcaa74620605edc42b600f2dde7ee14b7cc7
burgosfacundo <facundoandresbur@hotmail.com>
2025-06-06 12:15:11 -0300

    Change logica interfaz

--- mordisco-api/src/main/java/utn/back/mordiscoapi/service/interf/IUsuarioService.java
+++ mordisco-api/src/main/java/utn/back/mordiscoapi/service/interf/IUsuarioService.java
@@ -9,11 +9,11 @@ import utn.back.mordiscoapi.model.projection.UsuarioProjection;
 import java.util.List;
 
 public interface IUsuarioService {
-    void save(UsuarioDTO dto) throws BadRequestException;
+    void save(UsuarioDTO dto) throws BadRequestException, NotFoundException;
     List<UsuarioProjection> findAll();
     UsuarioProjection findById(Long id) throws NotFoundException;
     void delete(Long id) throws NotFoundException;
     void changePassword(String oldPassword, String newPassword, Long id) throws NotFoundException, BadRequestException;
-    List<UsuarioProjection> findByProjectRol(Long id) ;
+    List<UsuarioProjection> findByProjectRol(Long id) throws NotFoundException;
     void update(Long id, UsuarioUpdateDTO dto) throws NotFoundException, BadRequestException;
 }
