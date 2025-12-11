5c95940a3ce683ac625dff7a5c79897398ff7695
burgosfacundo <facundoandresbur@gmail.com>
2025-12-11 19:24:53 -0300

    agrego promocion en card de producto de mi menu

--- mordisco-front/src/app/shared/components/producto-card-component/producto-card-component.ts
+++ mordisco-front/src/app/shared/components/producto-card-component/producto-card-component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import ProductoCard from '../../models/producto/producto-card';
 
 @Component({
   selector: 'app-producto-card-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './producto-card-component.html'
 })
 export class ProductoCardComponent {
