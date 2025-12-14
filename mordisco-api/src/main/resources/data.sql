c4791b13aa96dd57b21fa44b2ef6930a63bdc597
burgosfacundo <facundoandresbur@gmail.com>
2025-12-14 16:50:07 -0300

    modifico docker compose y script

--- mordisco-api/src/main/resources/data.sql
+++ mordisco-api/src/main/resources/data.sql
@@ -5,8 +5,8 @@
 INSERT INTO `roles` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_CLIENTE'),(3,'ROLE_RESTAURANTE'),(4,'ROLE_REPARTIDOR');
 
 -- 2) configuracion_sistema
-INSERT INTO configuracion_sistema (id, porcentaje_ganancias_restaurante, costo_base_delivery, costo_por_kilometro, modo_mantenimiento, monto_minimo_pedido, porcentaje_ganancias_repartidor, radio_maximo_entrega, tiempo_maximo_entrega,fecha_actualizacion)
-VALUES (1, 80.0, 2000.0, 500.0, false, 5000.0, 80.0, 10.0, 45,'2025-11-28 08:28:49.000000');
+INSERT INTO configuracion_sistema (id, porcentaje_ganancias_restaurante, costo_base_delivery, costo_por_kilometro, monto_minimo_pedido, porcentaje_ganancias_repartidor, radio_maximo_entrega,fecha_actualizacion)
+VALUES (1, 80.0, 2000.0, 500.0, 5000.0, 80.0, 10.0,'2025-11-28 08:28:49.000000');
 
 -- 3) usuarios
 INSERT INTO usuarios (id, nombre, apellido, telefono, email, password, baja_logica,rol_id)
